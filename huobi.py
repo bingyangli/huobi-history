88ffbdc0d8a70d71d0bd4b043b16da68ef8d4a8f
clc <libingyang1228@gmail.com>
2018-09-23 14:38:15 +0800

    update

--- huobi.py
+++ huobi.py
@@ -3,11 +3,14 @@ import gzip
 import time
 import json
 import csv
-import pymysql
+
+import pymongo
+from pipelines import MongoDBPipeline
+
 
 global globalTime
 # 获取历史数据起始时间
-globalTime = 1526392680
+globalTime = 1512057600
 #1506787200
 
 
@@ -29,37 +32,7 @@ def loop_data(o, k=''):
         json_ob[k][c_line] = o
 
 
-def get_title_rows(json_ob):
-    title = []
-    row_num = 0
-    # global rows
-    rows = []
-    for key in json_ob:
-        title.append(key)
-        v = json_ob[key]
-        if len(v) > row_num:
-            row_num = len(v)
-        continue
-    for i in range(row_num):
-        row = {}
-        for k in json_ob:
-            v = json_ob[k]
-            if i in v.keys():
-                row[k] = v[i]
-            else:
-                row[k] = ''
-        rows.append(row)
-    for dbRow in rows:
-        sql = """insert into zecusdt_min(id,open,close,low,high,amount,vol,count) VALUES (""" + str(
-            dbRow.get('id')) + """,""" + str(dbRow.get('open')) + """,""" + str(dbRow.get('close')) + """,""" + str(
-            dbRow.get('low')) + """,""" + str(dbRow.get('high')) + """,""" + str(
-            dbRow.get('amount')) + """,""" + str(dbRow.get('vol')) + """,""" + str(dbRow.get('count')) + """)"""
-        try:
-            cursor.execute(sql)
-            db.commit()
-        except:
-            db.rollback()
-    return title, rows
+
 
 
 def write_csv(title, rows, csv_file_name):
@@ -82,28 +55,37 @@ global count
 count = 0
 global x
 x = globalTime
+interval = 18000
 if __name__ == '__main__':
+    server = '192.168.0.103'
+    port = 27017
+    db = "huobi"
+    name = ''
+    passwd = ''
+    col = '1day'    
+    
     while (1):
         try:
-            ws = create_connection("wss://api.huobi.pro/ws")
+            ws = create_connection("wss://api.huobi.br.com/ws")
             # ws = create_connection("wss://api.huobipro.com/ws")
 
             while (1):
-                globalTime += 18000
-                tradeStr = """{"req": "market.zecusdt.kline.1min","id": "id10", "from": """ + str(
-                    globalTime) + """, "to":""" + str(globalTime + 18000) + """ }"""
+                
+                tradeStr = """{"req": "market.zecusdt.kline.1day","id": "id10", "from": """ + str(
+                    globalTime) + """, "to":""" + str(1533536947) + """ }"""
                 ws.send(tradeStr)
                 compressData = ws.recv()
                 if compressData != '':
                     result = gzip.decompress(compressData).decode('utf-8')
+                    f=0
                 else:
                     print("正在重新连接")
-                    ws.connect("wss://api.huobi.pro/ws")
+                    ws.connect("wss://api.huobi.br.com/ws")
                     # ws.connect("wss://api.huobipro.com/ws")
 
-                    globalTime = x + (18000 * (count + 1))
-                    tradeStr2 = """{"req": "market.zecusdt.kline.1min","id": "id10", "from": """ + str(
-                        globalTime) + """, "to":""" + str((globalTime + 18000)) + """ }"""
+                    globalTime = x + (interval * (count + 1))
+                    tradeStr2 = """{"req": "market.zecusdt.kline.1day","id": "id10", "from": """ + str(
+                        globalTime) + """, "to":""" + str((globalTime + interval)) + """ }"""
                     ws.send(tradeStr2)
                     compressData = ws.recv()
                     result = gzip.decompress(compressData).decode('utf-8')
@@ -115,12 +97,13 @@ if __name__ == '__main__':
                 else:
                     resutlJson = json.loads(result)
                     data = resutlJson['data']
-                    db = pymysql.connect("192.168.1.167", "root", "123", "test3")
-                    cursor = db.cursor()
-                    json_to_csv(data)
+                    
+                    
+                    #json_to_csv(data)
                     count += 1
-                    db.close()
+                    
                     print(data)
+                globalTime += interval
         except:
             print('connect ws error,retry...')
             break
